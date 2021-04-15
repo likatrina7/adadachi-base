e54595deb9b10cc8a97b53ece54e6300220e8bdb
Katrina Li <likatrina7@gmail.com>
2021-04-15 15:20:49 -0700

    Created Game class.

--- game.py
+++ game.py
@@ -1,47 +1,55 @@
-from player import Player
-from adadachi import Adadachi
-from constants import *
 import random
 
-player = Player()
+class Game:
+    def __init__(self):
+        self.result = None
 
-def display(statement):
-    print(statement)
+    def left_or_right(self):
+        player_guess = input("let's play left or right! Please make a guess: \n\n").lower()
+        direction = random.choice(("left", "right"))
 
+        if direction == player_guess.lower():
+            print(f"\nYour guess {player_guess.upper()} is correct, you won the game!")
+            return True  
+        else:
+            print(f"\nYour guess {player_guess.upper()} is wrong, you lose the game!")
+            return False
 
-def create_adadachi():
-    name = input(GET_NAME + "\n\t")
-    foods = player.inventory["foods"]
-    games = player.inventory["games"]
-    personality = {
-        "fav_food": random.randint(0,len(foods)),
-        "fav_game": random.randint(0,len(games)),
-        "hates_food": random.randint(0,len(foods)),
-        "hates_game": random.randint(0,len(games)),
-    }
-    player.adadachi = Adadachi(name,personality)
+    def rock_scissors_paper(self):
+        player_input = input("let's play rock scissors paper!\n\nEnter rock/scissors/paper:\n\n").lower()
+        random_rsp = random.choice(("rock", "scissors", "paper"))
 
+        if player_input == random_rsp:
+            print("It's a tie!")
+        elif player_input == "scissors":
+            if random_rsp == "rock":
+                print (f"\nYour opponent played {random_rsp}, you lose the game!")
+                return False
+            else:
+                print (f"\nYour opponent played {random_rsp}, you won the game!")
+                return True
+        elif player_input == "rock":
+            if random_rsp == "scissors":
+                print (f"\nYour opponent played {random_rsp}, you won the game!")
+                return True
+            else:
+                print (f"\nYour opponent played {random_rsp}, you lose the game!")
+                return False
+        elif player_input == "paper":
+            if random_rsp == "scissors":
+                print (f"\nYour opponent played {random_rsp}, you lose the game!")
+                return False
+            else:
+                print (f"\nYour opponent played {random_rsp}, you won the game!")
+                return True
 
-def start_game():
-    display(TITLE)
-    answer = input(GREETING)
-    if answer.lower() == "y":
-        create_adadachi()
-        while player.adadachi.hunger < 5:
-            option = input(OPTIONS).lower()
+    def roll_dice(self):
+        print("let's play roll dice! If you get 6 you win!\n\n\t Rolling dice....\n")
+        num = random.randint(1, 6)
 
-            if option == "s":
-                player.get_status()
-            elif option == "c":
-                    player.clean()
-            elif option == "f":
-                if player.inventory["foods"]:
-                    player.feed()
-                else:
-                    break
-            elif option == "p":
-                player.play_with_adadachi()
-            elif option == "x":
-                return display(EXIT)
-        
-        display(LOST)
+        if num == 6:
+            print(f"Your got 6, you won the game!")
+            return True  
+        else:
+            print(f"Your got {num}, you lose the game!")
+            return False
